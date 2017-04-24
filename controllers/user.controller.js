723954866b36027baa4ff48fabe42492c70e4c73
iddiaz <diazdiazivan@gmail.com>
2017-04-24 16:06:08 +0200

    User controller creando metodo de login

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -7,7 +7,7 @@ function pruebas(req, res)  {
   res.status(200).send({message:'El controlador de usuario responde correctamente'});
 }
 
-// metodo guardar usuario
+// metodo guardar usuario NOTA: no se ha considerado aun si el usuario ya esta creado
 function saveUser( req, res ){
   let user = new User();
   //guardamos los parametros que vienen por post en el body en una variable por comodidad
@@ -51,10 +51,32 @@ function saveUser( req, res ){
   }
 }
 
+function loginUser(req, res) {
+  let params = req.body;
+  let email = params.email;
+  let password = params.password;
 
+  User.findOne({email: email.toLowerCase()}, (err, user) =>{
+    if (err) return res.status(500).send({message: `Error en la petición: ${err}`});
+
+    if(!user) return res.status(404).send({message: 'El usuario no existe en la base de datos (estas dando demasiadas pistas...mejor que pongas lo mismo el usuario no ha posido loguearse :-) )'});
+
+    bcrypt.compare(password, user.password, (err, check)=>{
+      if (err || !check) return res.status(403).send({message: 'El usuario no ha podido loguearse'});
+
+      if(params.gethash){
+        // devolver un token de jwt
+      }
+
+      return res.status(200).send({user});
+    });
+  });
+
+}
 
 
 module.exports = {
   pruebas,
-  saveUser
+  saveUser,
+  loginUser
 };
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -7,5 +7,6 @@ const api = express.Router();
 
 api.get('/prueba-controlador', UserController.pruebas);
 api.post('/register', UserController.saveUser);
+api.post('/login', UserController.loginUser);
 
 module.exports = api;
