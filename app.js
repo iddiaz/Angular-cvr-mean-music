870a9c99c633c790afd0666914367dc9b9d68dad
iddiaz <diazdiazivan@gmail.com>
2017-04-26 21:25:56 +0200

    COnfiguración de cabeceras http COORS

--- app.js
+++ app.js
@@ -15,6 +15,15 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 // configuración de cabeceras http
+app.use((req,res)=>{
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With,Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+
+  next();
+
+});
 
 // ruta de prueba
 // app.get('/prueba', (req, res)=>{
