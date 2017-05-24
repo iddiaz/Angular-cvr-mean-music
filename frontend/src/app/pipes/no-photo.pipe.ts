47bc59f04837e3efc0005f9dd2cef546c3df7c41
iddiaz <diazdiazivan@gmail.com>
2017-05-24 18:23:26 +0200

    Ajustando el pipe de obtencion de fotos para artista y album

--- frontend/src/app/pipes/no-photo.pipe.ts
+++ frontend/src/app/pipes/no-photo.pipe.ts
@@ -1,24 +1,36 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { ArtistService } from './../services/artist.service';
+import { AlbumService } from '../services/album.service';
 
 @Pipe({
   name: 'noPhoto'
 })
 export class NoPhotoPipe implements PipeTransform {
 
-  url = `${this.artistService.urlPpal}/get-image-artist`;
+  urlArtistImage = `${this.artistService.urlPpal}/get-image-artist`;
+  urlAlbumImage = `${this.albumService.urlPpal}/get-image-album`;
   noImage = 'assets/noimage.png';
 
-  constructor(private artistService: ArtistService){}
+  constructor(private artistService: ArtistService, private albumService: AlbumService){}
 
   transform(value: any, args?: any): any {
+
+    // console.log('el valor recivido en el pipe', value);
+    // console.log('el valor recivido en el args', args);
 
 
     if(!value) {
       return `${this.noImage}`;
     }
+    // console.log('URL PARA LA IMAGEN DEL ARTISTA',`${this.urlArtistImage}/${value}`);
+    // console.log('URL PARA LA IMAGEN DEL ALBUM',`${this.urlAlbumImage}/${value}`);
 
-    return `${this.url}/${value}`;  
+    if(value && args === 'imageAlbum') {
+      return `${this.urlAlbumImage}/${value}`;
+    }
+    if (value && args === 'imageArtist') {
+      return `${this.urlArtistImage}/${value}`;
+    }
    
       
   }
